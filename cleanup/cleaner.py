9377dcfd4dea96fe4c657d3b8dac2c4aad1ecdda
Luís Tavares <up201809679@fe.up.pt>
2021-11-30 12:56:58 +0000

    Add REVOGADO parsing

--- cleanup/cleaner.py
+++ cleanup/cleaner.py
@@ -20,7 +20,7 @@ def sections(path):
     """
     df = pd.read_csv(path, index_col='id')
     df.name = df.name.str.strip()
-    df.parent_id = df.parent_id.fillna(-1) # root section has parent -1
+    df.parent_id = df.parent_id.fillna(-1)  # root section has parent -1
     df.parent_id = df.parent_id.astype(int)
     df.to_csv("data/cleanup/section.csv")
 
@@ -31,14 +31,20 @@ def articles(path):
     """
     df = pd.read_csv(path, index_col='id')
 
-    groups = df['title'].str.extract(r'Artigo\s+(?P<key>\d+)\.º(?:\s+|-(?P<extra>\w)\s+)\(?(?P<title>.*?)\)?$', expand=True)
-    groups['key'] = np.where(groups.extra.notna(), groups.key + " " + groups.extra, groups.key)
+    groups = df['title'].str.extract(
+        r'Artigo\s+(?P<key>\d+)\.º(?:\s+|-(?P<extra>\w)\s+)\(?(?P<title>.*?)\)?$', expand=True)
+    groups['key'] = np.where(groups.extra.notna(
+    ), groups.key + " " + groups.extra, groups.key)
     groups = groups.drop(['extra'], axis=1)
 
     df = df.drop(['title'], axis=1)
     df = df.join(groups)
 
-    # TODO REVOGADO
+    df['text'] = df["text"].apply(lambda text: "Revogado" if re.search(
+        r'^\s*\(?Revogado\.?\)?\.?\s*(?:\\n)?$', text.strip(), re.IGNORECASE) is not None else text)
+
+    df['state'].mask(df['text'] == 'Revogado', 'Revogado', inplace=True)
+    print(df.state.describe())
 
     # separate into points
     points = _create_points(df)
@@ -55,9 +61,6 @@ def article_versions(path):
     points = _create_points(df)
     points.to_csv("data/cleanup/article_version_point.csv")
 
-    df.details.fillna('', inplace=True)
-    df['date'] = df.apply(lambda row: _get_date(row.details), axis=1)
-
     df.to_csv("data/cleanup/article_version.csv")
 
 
@@ -73,9 +76,12 @@ def _create_points(articles):
 
     id = 1
 
+    # TODO cleanup revogado points
+
     for article_id, row in articles.iterrows():
         # sees if the text in the article has points
-        if not isinstance(row.text, float) and re.search(r'1\.', row.text): # TODO is instance temporary (nan)
+        # TODO is instance temporary (nan)
+        if not isinstance(row.text, float) and re.search(r'1\.', row.text):
             # if it has points, split it into points
             points = _create_points_text(row.text)
 
